d9aa8fc125a46d5e8b23a747d63c0675a0afa955
Gilang C. Syahputra <gilangchandra9@gmail.com>
2021-09-08 15:52:13 +0700

    initial setup environment

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Memanggil model
+// const db = require('./app/models');
+
+//Deklarasi express.js
+const app = express();
+
+//Membuat daftar web yang dapat mengonsumsi ValorantPI
+let whiteList = [
+    'http://localhost:8081'
+];
+
+//Mendaftarkan web yang dapat mengonsumsi ValorantPI
+let corsOption = {
+    origin: function (origin, callback) {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not Allowed by CORS!'));
+        }
+    }
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Migrasi tabel yang ada dalam setiap model
+// db.sequelize.sync({ force: false });
+
+//Inisasi routing pada halaman awal
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to ValorantPI!"
+    });
+});
+
+//Inisiasi port yang akan dipakai
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+})
